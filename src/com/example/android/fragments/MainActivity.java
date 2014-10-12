759c4c31383a73403348e675fc40f4fb1531c8b3
sathish <sathish@eggheadcreative.com>
2014-10-12 16:30:47 +0530

    minor refactor in on article selected

--- src/com/example/android/fragments/MainActivity.java
+++ src/com/example/android/fragments/MainActivity.java
@@ -48,27 +48,16 @@ public class MainActivity extends FragmentActivity
                 getSupportFragmentManager().findFragmentById(R.id.article_fragment);
 
         if (articleFrag != null) {
-            // If article frag is available, we're in two-pane layout...
-
-            // Call a method in the ArticleFragment to update its content
             articleFrag.updateArticleView(position);
 
         } else {
-            // If the frag is not available, we're in the one-pane layout and must swap frags...
-
-            // Create fragment and give it an argument for the selected article
             ArticleFragment newFragment = new ArticleFragment();
             Bundle args = new Bundle();
             args.putInt(ArticleFragment.ARG_POSITION, position);
             newFragment.setArguments(args);
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-            // Replace whatever is in the fragment_container view with this fragment,
-            // and add the transaction to the back stack so the user can navigate back
             transaction.replace(R.id.fragment_container, newFragment);
             transaction.addToBackStack(null);
-
-            // Commit the transaction
             transaction.commit();
         }
     }
